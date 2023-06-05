614475b8a2b3162edc60521c1bc7b52aa81db27a
akashnithish <sksmakash1000@gmail.com>
2023-06-05 14:00:12 +0000

    CRUD Operations were coded for extra specifications of the project

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.use(express.json());
 const dbPath = path.join(__dirname, "twitterClone.db");
 let db = null;
 
+// initalizing db and server
+
 const initializeDbAndServer = async () => {
   try {
     db = await open({
@@ -27,6 +29,71 @@ const initializeDbAndServer = async () => {
   }
 };
 initializeDbAndServer();
+
+//Check for CRUD operations
+
+//GET METHOD
+app.get("/", async (request, response) => {
+  const query = `SELECT * FROM user;`;
+  const getQuery = await db.all(query);
+  response.send(getQuery);
+});
+
+//GET METHOD (WITH ID)
+app.get("/twitter/user/:userId/", async (request, response) => {
+  const { userId } = request.params;
+  const getUserQuery = `
+    SELECT
+      *
+    FROM
+      user
+    WHERE
+      user_id = ${userId};`;
+  const userDetails = await db.get(getUserQuery);
+  response.send(userDetails);
+});
+
+//POST METHOD
+app.post("/user/register/", async (request, response) => {
+  const { username, password, name, gender } = request.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const createUserQuery = `INSERT INTO user(username,password,name,gender)
+       VALUES('${username}','${hashedPassword}','${name}','${gender}');`;
+  await db.run(createUserQuery);
+  response.send("User created successfully");
+});
+
+//PUT METHOD
+app.put("/user/update/:userId/", async (request, response) => {
+  const { userId } = request.params;
+  const userDetails = request.body;
+  const { username, password, name, gender } = userDetails;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const updateUserQuery = `
+    UPDATE
+      user
+    SET
+      name='${name}',
+      username= '${username}',
+      password= '${hashedPassword}',
+      gender= '${gender}'
+    WHERE
+      user_id = ${userId};`;
+  await db.run(updateUserQuery);
+  response.send("User Details Updated Successfully");
+});
+
+//DELETE METHOD
+app.delete("/user/delete/:userId/", async (request, response) => {
+  const { userId } = request.params;
+  const deleteUserQuery = `
+    DELETE FROM
+      user
+    WHERE
+      user_id = ${userId};`;
+  await db.run(deleteUserQuery);
+  response.send("User Details Deleted Successfully");
+});
 
 // GETTING USER FOLLOWING PEOPLE ID'S
 
@@ -93,7 +160,7 @@ WHERE tweet.tweet_id = '${tweetId}' AND follower_user_id = '${userId}';`;
 
 app.post("/register/", async (request, response) => {
   const { username, password, name, gender } = request.body;
-  const getUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
+  const getUserQuery = `SELECT * FROM user WHERE username = '${username}';`;
   const userDBDetails = await db.get(getUserQuery);
 
   if (userDBDetails !== undefined) {
@@ -106,7 +173,7 @@ app.post("/register/", async (request, response) => {
     } else {
       const hashedPassword = await bcrypt.hash(password, 10);
       const createUserQuery = `INSERT INTO user(username,password,name,gender)
-       VALUES('${username}','${hashedPassword}','${name}','${gender}')`;
+       VALUES('${username}','${hashedPassword}','${name}','${gender}');`;
       await db.run(createUserQuery);
       response.send("User created successfully");
     }
